7595869275742d57ab8b76e64a7b32e34d12a286
Rick Dörr <Rick.nl@hotmail.com>
2021-03-31 14:24:10 +0200

    Simple Ragdol.
    Ragdol voor rick student.

--- Ricks 2D Unity Game/Assets/Scripts/PlayerController.cs
+++ Ricks 2D Unity Game/Assets/Scripts/PlayerController.cs
@@ -5,25 +5,35 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     public float speed = 5f;
     public float jumpSpeed = 8f;
-    public AudioSource jump;
-    public Transform groundCheckPoint;
     public float groundCheckRadius;
     public LayerMask groundLayer;
+    public Rigidbody2D rigidbody;
+    public bool jumpable = false;
     
     // Use this for initialization
     void Start()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+       
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //move left & right
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * 5);
+       
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckRadius, groundLayer);
+        if (hit.collider != null){
+            jumpable = true;
+            Debug.Log(hit.point);
+        }
+
+        if (jumpable)
+        {
+            rigidbody.velocity = Vector2.up * jumpSpeed * 20;
+            jumpable = false;
+        }
+
+        
         
     }
 }
